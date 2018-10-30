26ea1bbe1596b77aa8c0299d40d66c15ab6a9b28
Animesh Dutta <34543420+animesh-dutta@users.noreply.github.com>
2018-10-30 22:20:00 +0530

    Update Hello.java

--- src/com/animeshdutta/Hello.java
+++ src/com/animeshdutta/Hello.java
@@ -9,10 +9,8 @@ package com.animeshdutta;
  */
 public class Hello {
 
-	public Hello() {
-		
+	public Hello() {	
 	}
-	
 	public static void main(String[] args) {
 		System.out.println("How are you doing ?");
 	}
